ffcd4dedf7c5b334a1d0cf412043d4bbe2f75a05
Alejandro <ajgarciabouzada@gmail.com>
2023-11-16 20:28:21 -0300

    Agregar archivos de proyecto.

---
+++ Entidades/Practicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum EPracticas { Biodecodificacion, Reiki, Tarot };
+    public class Practicas
+    {
+        private static Dictionary<EPracticas, string> practicas;
+
+        private string practica;
+        private string descripcion;
+        private double precio;
+
+        static Practicas()
+        {
+            practicas = new Dictionary<EPracticas, string>();
+        }
+        
+        public Practicas(EPracticas practica, string descripcion )
+        {
+            this.practica = practica.ToString();
+            this.descripcion = descripcion;
+        }
+    }
+}
